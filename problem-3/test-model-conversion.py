e4da790ce3c50c40f284b1ba36c1ebfe4b65179b
izakharkin <ilyazaharkin@gmail.com>
2025-06-11 15:58:48 +0300

    add problem3 example

--- problem-3/test-model-conversion.py
+++ problem-3/test-model-conversion.py
@@ -1,8 +1,18 @@
-import coremltools as ct
+import argparse
+
 import torch
-import torch.nn.functional as F
 from torch import nn
 from torch.fft import irfftn, rfftn
+import torch.nn.functional as F
+
+import coremltools as ct
+from coremltools.optimize.torch.quantization import PostTrainingQuantizerConfig, PostTrainingQuantizer
+
+
+class CustomConv2d(nn.Conv2d):
+    def _conv_forward(self, input: torch.Tensor, weight: torch.Tensor, bias: torch.Tensor | None) -> torch.Tensor:
+        weight = weight ** 2
+        return super()._conv_forward(input, weight, bias)
 
 
 class SimpleConvBlock(nn.Module):
@@ -14,9 +24,9 @@ class SimpleConvBlock(nn.Module):
     def __init__(self, in_ch: int, out_ch: int):
         super().__init__()
         self.ffn = nn.Sequential(
-            nn.Conv2d(in_ch, out_ch, kernel_size=3, padding=1),
+            CustomConv2d(in_ch, out_ch, kernel_size=3, padding=1),
             nn.GELU(),
-            nn.Conv2d(out_ch, out_ch, kernel_size=3, padding=1),
+            CustomConv2d(out_ch, out_ch, kernel_size=3, padding=1),
             nn.GELU(),
         )
 
@@ -32,14 +42,14 @@ class SimpleFFCBlock(nn.Module):
 
     def __init__(self, in_ch: int, out_ch: int):
         super().__init__()
-        self.pre = nn.Conv2d(in_ch, out_ch, kernel_size=1)
+        self.pre = CustomConv2d(in_ch, out_ch, kernel_size=1)
         self.ffn = nn.Sequential(
-            nn.Conv2d(2 * out_ch, 2 * out_ch, kernel_size=3, padding=1),
+            CustomConv2d(2 * out_ch, 2 * out_ch, kernel_size=3, padding=1),
             nn.GELU(),
-            nn.Conv2d(2 * out_ch, 2 * out_ch, kernel_size=3, padding=1),
+            CustomConv2d(2 * out_ch, 2 * out_ch, kernel_size=3, padding=1),
             nn.GELU(),
         )
-        self.post = nn.Conv2d(out_ch, out_ch, kernel_size=1)
+        self.post = CustomConv2d(out_ch, out_ch, kernel_size=1)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         out = self.pre(x)
@@ -70,15 +80,15 @@ class SimpleTransformerBlock(nn.Module):
     def __init__(self, in_ch: int, out_ch: int):
         super().__init__()
         # Attention part.
-        self.q = nn.Conv2d(in_ch, self._NUM_HEADS * self._QK_DIM, kernel_size=1)
-        self.k = nn.Conv2d(in_ch, self._NUM_HEADS * self._QK_DIM, kernel_size=1)
-        self.v = nn.Conv2d(in_ch, self._NUM_HEADS * self._V_DIM, kernel_size=1)
-        self.proj = nn.Conv2d(self._NUM_HEADS * self._V_DIM, out_ch, kernel_size=1)
+        self.q = CustomConv2d(in_ch, self._NUM_HEADS * self._QK_DIM, kernel_size=1)
+        self.k = CustomConv2d(in_ch, self._NUM_HEADS * self._QK_DIM, kernel_size=1)
+        self.v = CustomConv2d(in_ch, self._NUM_HEADS * self._V_DIM, kernel_size=1)
+        self.proj = CustomConv2d(self._NUM_HEADS * self._V_DIM, out_ch, kernel_size=1)
         # Feed-forward part.
         self.ffn = nn.Sequential(
-            nn.Conv2d(out_ch, out_ch * 4, kernel_size=1),
+            CustomConv2d(out_ch, out_ch * 4, kernel_size=1),
             nn.GELU(),
-            nn.Conv2d(out_ch * 4, out_ch, kernel_size=1),
+            CustomConv2d(out_ch * 4, out_ch, kernel_size=1),
         )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
@@ -108,7 +118,7 @@ class UpsamplingBlock(nn.Module):
         return F.interpolate(x, scale_factor=2, mode='nearest')
 
 
-class SimpleTransformerModel(nn.Module):
+class SimpleTransformerModelWithCustomConv2d(nn.Module):
 
     """This is a simple transformer model for demonstration purposes."""
 
@@ -143,8 +153,8 @@ class SimpleTransformerModel(nn.Module):
         super().__init__()
         self.blocks = nn.Sequential(
             # Stem.
-            nn.Conv2d(3, 20, kernel_size=3, stride=2, padding=1),
-            nn.Conv2d(20, 40, kernel_size=3, stride=2, padding=1),
+            CustomConv2d(3, 20, kernel_size=3, stride=2, padding=1),
+            CustomConv2d(20, 40, kernel_size=3, stride=2, padding=1),
             # Encoder Body.
             self._build_conv_blocks(num_blocks=5, in_ch=40, out_ch=64),
             DownsamplingBlock(),
@@ -162,9 +172,9 @@ class SimpleTransformerModel(nn.Module):
             self._build_ffc_blocks(num_blocks=2, in_ch=64, out_ch=40),
             # Head.
             UpsamplingBlock(),
-            nn.Conv2d(40, 20, kernel_size=3, padding=1),
+            CustomConv2d(40, 20, kernel_size=3, padding=1),
             UpsamplingBlock(),
-            nn.Conv2d(20, 3, kernel_size=3, padding=1),
+            CustomConv2d(20, 3, kernel_size=3, padding=1),
             nn.Sigmoid(),
         )
 
@@ -172,15 +182,36 @@ class SimpleTransformerModel(nn.Module):
         return self.blocks(x)
 
 
-if __name__ == '__main__':
+parser = argparse.ArgumentParser(description="Test model conversion to CoreML")
+parser.add_argument("--quantize", action="store_true", help="Whether to apply quantization to the model.")
 
-    model = torch.jit.trace(SimpleTransformerModel(), example_inputs=[torch.randn(1, 3, 1024, 1024)])
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    model = SimpleTransformerModelWithCustomConv2d()
+    if args.quantize:
+        qtz_cfg = PostTrainingQuantizerConfig.from_dict({
+            "global_config": {
+                "weight_dtype": "int8",
+                "granularity": "per_channel",
+            },
+            "module_name_configs": {},
+        })
+        quantizer = PostTrainingQuantizer(model, qtz_cfg)
+        model = quantizer.compress()
+    model = torch.jit.trace(
+        model.eval().requires_grad_(False),
+        example_inputs=[torch.randn(1, 3, 1024, 1024)],
+    )
     coreml_model = ct.convert(
-        model=model, source="pytorch",
+        model=model,
+        source="pytorch",
         inputs=[ct.ImageType("input_image", shape=(1, 3, 1024, 1024))],
         outputs=[ct.ImageType("output_image")],
-        minimum_deployment_target=ct.target.iOS16, convert_to="mlprogram",
+        minimum_deployment_target=ct.target.iOS16,
+        convert_to="mlprogram",
         compute_precision=ct.precision.FLOAT16,
         compute_units=ct.ComputeUnit.ALL,
     )
-    coreml_model.save("/workdir/storage/problem1.mlpackage")
+    output_filepath = "problem3.mlpackage" if not args.quantize else "problem3_quant.mlpackage"
+    coreml_model.save(output_filepath)
